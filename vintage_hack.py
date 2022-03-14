7dbebc58ed7b8e8f9a0f5b81d4b00abf44c4b38e
laddi <meladbrook@gmail.com>
2022-03-13 20:58:39 -0700

    Got basic word input working

--- vintage_hack.py
+++ vintage_hack.py
@@ -17,6 +17,10 @@ grid = np.full((x, y), '')
 words = random.sample(words_five, number_of_words)
 print(words)
 
+
+
+
+
 for word in words:
     word = [letter for letter in word]
     starting_point = (random.randint(0, x-1), random.randint(0, y-1))
@@ -26,11 +30,17 @@ for word in words:
         try:
             grid[starting_point[0], starting_point[1] + i] = word[i]
         except IndexError:
-            grid[starting_point[0] + 1, [0]] = word[i]
-            break_point = 1
+            #grid[starting_point[0] + 1, [0]] = word[i]
+            break_point = i
+            print(break_point)
+            word = word[break_point:]
             break
     if break_point > 0:
-        for i in range(break_point, len(word)):
-            grid[starting_point[0] + 1, [0 + 1]] = word[i]
+        for i in range(0, len(word)):
+            try:
+                grid[starting_point[0] + 1, [0 + i]] = word[i]
+            except IndexError:
+                print("System Error: Please attempt again.")
+                break
 
 print(grid)
