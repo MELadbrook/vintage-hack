be503ed5f99cb0409edff2971e460122cd257fc3
laddi <meladbrook@gmail.com>
2022-03-17 23:21:46 -0700

    Added starting points function

--- vintage_hack.py
+++ vintage_hack.py
@@ -8,7 +8,7 @@ words_five = {'pleat', 'hello', 'tears', 'mouse'}
 
 x = 10
 y = 20
-number_of_words = 2
+number_of_words = 4
 
 punctuation = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', ':', ';', '"',
                '{', '}', '[', ']', '+', '=']
@@ -16,6 +16,7 @@ punctuation = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', ':', ';', '"',
 grid = np.full((x, y), '')
 
 words = random.sample(words_five, number_of_words)
+length_of_word = len(words[0])
 print(words)
 
 used_coords = []
@@ -59,18 +60,55 @@ grid_index = create_index(grid)
 #print(back_fill(grid))
 #create_grid(x, grid)
 
-print("Character from grid: {}".format(grid[grid_index[0]]))
+#print("Character from grid: {}".format(grid[grid_index[0]]))
 
 
-for item in words:
-    word = [letter for letter in item]
-    starting_point = random.randint(0, len(grid_index) - len(word))
+
+#starting_points = random.sample(range(len(grid_index)-(len(words)-1)*(length_of_word)+1), len(words))
+#starting_points = np.cumsum(np.ones((len(words),), np.int) * (length_of_word+1) + np.random.randint(0, 200, (len(words),))) - (length_of_word+1)
+'''
+while True:
+  stuff()
+  if fail_condition:
+    break'''
+
+
+
+def create_starting_points():
+    starting_points = random.sample(range(len(grid_index)), len(words))
+    starting_points.sort()
+    diffs = [j-i for i, j in zip(starting_points[:-1], starting_points[1:])]
+    print(diffs)
+    for i in diffs:
+        print("l")
+        if i < length_of_word + 1:
+            #break
+            return create_starting_points()
+    return starting_points
+
+#print(create_starting_points())
+
+
+starting_points = create_starting_points()
+
+for item in range(len(words)):
+    word = [letter for letter in words[item]]
+    starting_point = starting_points[item]
+    #for point in starting_points:
+     #   if starting_point - point < len(word) | starting_point - point != 0:
+
+
     grid[grid_index[starting_point]] = word[0]
     for i in range(1, len(word)):
         grid[grid_index[starting_point + i]] = word[i]
 
 
 print(grid)
+
+
+#print(random.sample(range(len(grid_index)), len(words)))
+#print(random.sample(range(len(grid_index)-(len(words)-1)*(len(word)+1)), len(words)))
+
 '''
 for word in words:
     word = [letter for letter in word]
